a8666de3208b7f71ba25e1b603515414097248e9
banga87 <angusroymcdonald@gmail.com>
2022-11-03 12:56:43 +1100

    added 404 handling

--- store/serializers.py
+++ store/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.models.CharField(max_length=255)
+    title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
