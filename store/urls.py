8fcf321f6cd5eb4458e75e7980c69ee4ae886faf
banga87 <angusroymcdonald@gmail.com>
2022-11-07 12:53:49 +1100

    played with HyperlinkRelatedField()

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.products_list),
-    path('products/<int:id>', views.product_details)
+    path('products/<int:id>', views.product_details),
+    path('collections/<int:pk>', views.collection_detail, name='collection-detail')
 ]
