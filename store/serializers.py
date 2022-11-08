8827a83388a7f52e205da7fa2edbe74242979216
banga87 <angusroymcdonald@gmail.com>
2022-11-08 16:47:36 +1100

    create and overriding

--- store/serializers.py
+++ store/serializers.py
@@ -9,16 +9,8 @@ class CollectionSeralizer(serializers.Serializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'price_with_tax', 'collection']
-
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+        fields = ['id', 'title', 'slug', 'description', 'unit_price', 'inventory', 'collection', 'price_with_tax']
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset= Collection.objects.all(),
-    #     view_name='collection-detail',    
-    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
