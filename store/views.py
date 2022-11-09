5bff8ef03a6ef0dbb7c56a363b2d1facf78511ad
banga87 <angusroymcdonald@gmail.com>
2022-11-09 15:23:48 +1100

    ordered imports

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
