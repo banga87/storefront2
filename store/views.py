bd0afbb17940ae9750cbe3818e875768431bea91
BingBong <angusroymcdonald@gmail.com>
2022-12-23 14:53:14 +1100

    updated permissions for the OrderViewSet

--- store/views.py
+++ store/views.py
@@ -136,7 +136,12 @@ class CustomerViewSet(ModelViewSet):
 Orders Views
 """
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = CreateOrderSerializer(
